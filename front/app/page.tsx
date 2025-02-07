6d90974f388f3f9bc166580ae2997f3787781995
theocampbell <57679708+theocampbell@users.noreply.github.com>
2025-02-07 12:13:14 +0100

    [FRONT] test ci/cd (#18)

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div className='global-margin flex h-screen flex-col items-center justify-center'>
-      <h1>Welcome to Eclaireur Public</h1>
+      <h1>
+        testing
+      </h1>
     </div>
   );
 }
