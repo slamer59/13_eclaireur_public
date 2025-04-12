69735c5b897d9caefe232e3df4fe84d30d7788f4
GuillaumeFo <43350523+GuillaumeFo@users.noreply.github.com>
2025-04-12 15:31:58 +0200

    [BACK] MP enricher de type_prix et forme_prix (#166)

--- back/scripts/enrichment/marches_enricher.py
+++ back/scripts/enrichment/marches_enricher.py
@@ -6,6 +6,7 @@ from back.scripts.datasets.marches import MarchesPublicsWorkflow
 from back.scripts.enrichment.base_enricher import BaseEnricher
 from back.scripts.enrichment.utils.cpv_utils import CPVUtils
 from back.scripts.utils.dataframe_operation import normalize_montant
+import json
 
 
 class MarchesPublicsEnricher(BaseEnricher):
@@ -27,6 +28,9 @@ class MarchesPublicsEnricher(BaseEnricher):
     def _clean_and_enrich(cls, inputs: typing.List[pl.DataFrame]) -> pl.DataFrame:
         # Data analysts, please add your code here!
         marches, cpv_labels, *_ = inputs
+
+        marches = marches.pipe(cls.forme_prix_enrich).pipe(cls.type_prix_enrich)
+
         # do stuff with sirene
         marches_pd = (
             marches.to_pandas()
@@ -36,3 +40,50 @@ class MarchesPublicsEnricher(BaseEnricher):
             )  # distribute montant evenly when more than one contractor
         )
         return pl.from_pandas(marches_pd).pipe(CPVUtils.add_cpv_labels, cpv_labels=cpv_labels)
+
+    @staticmethod
+    def forme_prix_enrich(marches: pl.DataFrame) -> pl.DataFrame:
+        return marches.with_columns(
+            pl.when(pl.col("formePrix") == "Ferme, actualisable")
+            .then(pl.lit("Ferme et actualisable"))
+            .when(pl.col("formePrix") == "")
+            .then(pl.lit(None))
+            .otherwise(pl.col("formePrix"))
+            .alias("forme_prix")
+        )
+
+    @staticmethod
+    def safe_typePrix_json_load(x):
+        try:
+            parsed = json.loads(x)
+            if isinstance(parsed, list) and parsed:
+                return parsed[0]
+            elif isinstance(parsed, dict):
+                type_prix = parsed.get("typePrix")
+                if isinstance(type_prix, list) and type_prix:
+                    return type_prix[0]
+                if isinstance(type_prix, str):
+                    return type_prix
+            return None
+        except (json.JSONDecodeError, TypeError):
+            return None
+
+    @staticmethod
+    def type_prix_enrich(marches: pl.DataFrame) -> pl.DataFrame:
+        return (
+            marches.with_columns(
+                pl.col("typesPrix").map_elements(
+                    MarchesPublicsEnricher.safe_typePrix_json_load, return_dtype=pl.Utf8
+                )
+            )
+            .with_columns(
+                pl.coalesce(pl.col(["typesPrix", "typePrix", "TypePrix"])).alias("typePrix")
+            )
+            .with_columns(
+                pl.when((pl.col("typePrix") == "") | (pl.col("typePrix") == "NC"))
+                .then(pl.lit(None))
+                .otherwise(pl.col("typePrix"))
+            )
+            .rename({"typePrix": "type_prix"})
+            .drop(["typesPrix", "TypePrix"])
+        )
