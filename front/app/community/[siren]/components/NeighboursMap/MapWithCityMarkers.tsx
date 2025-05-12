1ba25060053fca5377b9f1cce3106e38c869f7ee
OctaveLT <99271452+OctaveLT@users.noreply.github.com>
2025-05-12 15:54:55 +0200

    [FRONT] fix: block map interaction for neighbours map  (#232)
    Co-authored-by: OctaveLT <ole_tullier@orange.fr>

--- front/app/community/[siren]/components/NeighboursMap/MapWithCityMarkers.tsx
+++ front/app/community/[siren]/components/NeighboursMap/MapWithCityMarkers.tsx
@@ -69,6 +69,7 @@ export default function MapWithCityMarkers({ center, cities }: MapWithCityMarker
         longitude: center[1],
       }}
       mapStyle={MAP_STYLE_URL}
+      interactive={false}
       style={style}
     >
       {markers}
