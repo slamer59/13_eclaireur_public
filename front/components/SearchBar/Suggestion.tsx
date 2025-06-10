0945d23021d7f08b9289e1cb02cf52ba0cffe809
Olivier <olivier.pretre@gmx.fr>
2025-06-10 15:39:39 +0200

    [FRONT] search bar modif US-0102 (#262)

--- front/components/SearchBar/Suggestion.tsx
+++ front/components/SearchBar/Suggestion.tsx
@@ -1,12 +1,16 @@
 import { Community } from '@/app/models/community';
+import { stringifyCommunityType } from '@/utils/utils';
 
-type CommunitySuggestionDisplayProps = Pick<Community, 'nom' | 'code_postal'>;
+type CommunitySuggestionDisplayProps = Pick<Community, 'nom' | 'code_postal' | 'type'>;
 
-export default function Suggestion({ nom, code_postal }: CommunitySuggestionDisplayProps) {
+export default function Suggestion({ nom, code_postal, type }: CommunitySuggestionDisplayProps) {
+  const communityTypeText = stringifyCommunityType(type);
   return (
-    <span>
-      {nom}
-      {code_postal && <> – {code_postal}</>}
-    </span>
+    <div className='flex w-full justify-between'>
+      <span>
+        {nom} {code_postal && <span> – {code_postal}</span>}
+      </span>
+      {communityTypeText && <span> {communityTypeText}</span>}
+    </div>
   );
 }
