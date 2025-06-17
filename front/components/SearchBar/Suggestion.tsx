836d31cd8cdbb49c7b9075a151fb7b3e9617c99c
Olivier <olivier.pretre@gmx.fr>
2025-06-17 18:05:44 +0200

    [Front] searchbar modif2 add siren code (#264)

--- front/components/SearchBar/Suggestion.tsx
+++ front/components/SearchBar/Suggestion.tsx
@@ -1,16 +1,23 @@
 import { Community } from '@/app/models/community';
 import { stringifyCommunityType } from '@/utils/utils';
 
-type CommunitySuggestionDisplayProps = Pick<Community, 'nom' | 'code_postal' | 'type'>;
-
-export default function Suggestion({ nom, code_postal, type }: CommunitySuggestionDisplayProps) {
+type CommunitySuggestionDisplayProps = Pick<Community, 'nom' | 'code_postal' | 'type' | 'siren'>;
+export default function Suggestion({
+  nom,
+  code_postal,
+  type,
+  siren,
+}: CommunitySuggestionDisplayProps) {
   const communityTypeText = stringifyCommunityType(type);
   return (
     <div className='flex w-full justify-between'>
       <span>
-        {nom} {code_postal && <span> – {code_postal}</span>}
+        {nom} {code_postal && <>– {code_postal}</>}
       </span>
-      {communityTypeText && <span> {communityTypeText}</span>}
+      <span>
+        {communityTypeText && <> {communityTypeText}</>}
+        {siren && <span className='ml-8 text-[12px]'> siren : {siren}</span>}
+      </span>
     </div>
   );
 }
