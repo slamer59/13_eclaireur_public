750cb624359bb6eaa65913610b10712c9c40b07f
Thomas PEDOT <thomas.pedot@gmail.com>
2025-07-20 18:16:24 +0200

    WIP

--- front/backstop.config.js
+++ front/backstop.config.js
@@ -1,20 +1,64 @@
-// Function to generate scenarios from Storybook stories
-function generateStorybookScenarios() {
-  const scenarios = [];
-  
-  // Base scenarios for common Storybook stories
-  const commonStories = [
-    'ui-button--default',
-    'ui-button--secondary',
-    'ui-button--destructive',
-    'ui-button--outline',
-    'ui-button--ghost',
-    'ui-button--link',
-    'components-searchbar--default',
-    'pages-homepageheader--default'
-  ];
+/* eslint-disable */
+const getFetch = async () => {
+  const { default: fetch } = await import('node-fetch');
+  return fetch;
+};
 
-  for (const story of commonStories) {
+module.exports = (async () => {
+  const fetch = await getFetch();
+  return {
+    id: "eclaireur_public_visual_tests",
+    viewports: [
+      {
+        label: "phone",
+        width: 375,
+        height: 667
+      },
+      {
+        label: "tablet",
+        width: 768,
+        height: 1024
+      },
+      {
+        label: "desktop",
+        width: 1280,
+        height: 800
+      }
+    ],
+    onBeforeScript: "playwright/onBefore.js",
+    onReadyScript: "playwright/onReady.js",
+    scenarios: await generateStorybookScenarios(),
+    paths: {
+      bitmaps_reference: "backstop_data/bitmaps_reference",
+      bitmaps_test: "backstop_data/bitmaps_test",
+      engine_scripts: "backstop_data/engine_scripts",
+      html_report: "backstop_data/html_report",
+      ci_report: "backstop_data/ci_report",
+      tempCompareConfigFileName: "backstop_data/tempCompareConfigFileName.json"
+    },
+    report: ["browser"],
+    engine: "playwright",
+    engineOptions: {
+      args: ["--no-sandbox"]
+    },
+    asyncCaptureLimit: 5,
+    asyncCompareLimit: 50,
+    debug: false,
+    debugWindow: false
+  };
+})();
+
+async function fetchStorybookStories() {
+  const response = await fetch('http://localhost:6006/stories.json');
+  const data = await response.json();
+  return Object.keys(data.stories);
+}
+
+async function generateStorybookScenarios() {
+  const scenarios = [];
+  const stories = await fetchStorybookStories();
+
+  for (const story of stories) {
     scenarios.push({
       label: story,
       url: `http://localhost:6006/iframe.html?id=${story}&viewMode=story`,
@@ -28,42 +72,44 @@ function generateStorybookScenarios() {
   return scenarios;
 }
 
-module.exports = {
-  id: "eclaireur_public_visual_tests",
-  viewports: [
-    {
-      label: "phone",
-      width: 375,
-      height: 667
+module.exports = (async () => {
+  return {
+    id: "eclaireur_public_visual_tests",
+    viewports: [
+      {
+        label: "phone",
+        width: 375,
+        height: 667
+      },
+      {
+        label: "tablet",
+        width: 768,
+        height: 1024
+      },
+      {
+        label: "desktop",
+        width: 1280,
+        height: 800
+      }
+    ],
+    onBeforeScript: "playwright/onBefore.js",
+    onReadyScript: "playwright/onReady.js",
+    scenarios: await generateStorybookScenarios(),
+    paths: {
+      bitmaps_reference: "backstop_data/bitmaps_reference",
+      bitmaps_test: "backstop_data/bitmaps_test",
+      engine_scripts: "backstop_data/engine_scripts",
+      html_report: "backstop_data/html_report",
+      ci_report: "backstop_data/ci_report"
     },
-    {
-      label: "tablet",
-      width: 768,
-      height: 1024
+    report: ["browser"],
+    engine: "playwright",
+    engineOptions: {
+      args: ["--no-sandbox"]
     },
-    {
-      label: "desktop",
-      width: 1280,
-      height: 800
-    }
-  ],
-  onBeforeScript: "playwright/onBefore.js",
-  onReadyScript: "playwright/onReady.js",
-  scenarios: generateStorybookScenarios(),
-  paths: {
-    bitmaps_reference: "backstop_data/bitmaps_reference",
-    bitmaps_test: "backstop_data/bitmaps_test",
-    engine_scripts: "backstop_data/engine_scripts",
-    html_report: "backstop_data/html_report",
-    ci_report: "backstop_data/ci_report"
-  },
-  report: ["browser"],
-  engine: "playwright",
-  engineOptions: {
-    args: ["--no-sandbox"]
-  },
-  asyncCaptureLimit: 5,
-  asyncCompareLimit: 50,
-  debug: false,
-  debugWindow: false
-};
+    asyncCaptureLimit: 5,
+    asyncCompareLimit: 50,
+    debug: false,
+    debugWindow: false
+  };
+})();
